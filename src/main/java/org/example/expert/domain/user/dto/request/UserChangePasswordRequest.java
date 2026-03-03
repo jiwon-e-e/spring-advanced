93f1bb9627aca4e3963ef35b049c4dd16819943a
kwonjiwon <kjw81024@gmail.com>
2026-03-03 23:10:48 +0900

    level 3: N+1 problem solved by EntityGraph

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -16,6 +16,7 @@ public class UserChangePasswordRequest {
     @NotBlank
     private String oldPassword;
     @NotBlank
-    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).{8,}$", message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).{8,}$",
+            message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.")
     private String newPassword;
 }
