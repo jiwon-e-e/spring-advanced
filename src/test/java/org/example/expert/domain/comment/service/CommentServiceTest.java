77fc52dfd72d12f32b2b587fbd2b7337aac13cae
kwonjiwon <kjw81024@gmail.com>
2026-03-06 01:45:13 +0900

    level 7: test code - CommentService

--- src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
+++ src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.comment.service;
 
 import org.example.expert.domain.comment.dto.request.CommentSaveRequest;
+import org.example.expert.domain.comment.dto.response.CommentResponse;
 import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
 import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.comment.repository.CommentRepository;
@@ -11,18 +12,24 @@ import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CommentServiceTest {
@@ -34,8 +41,32 @@ class CommentServiceTest {
     @InjectMocks
     private CommentService commentService;
 
+
     @Test
-    public void comment_등록_중_할일을_찾지_못해_에러가_발생한다() {
+    @DisplayName("성공 - 댓글 생성")
+    public void saveComment_succeed() {
+        // given
+        long todoId = 1;
+        CommentSaveRequest request = new CommentSaveRequest("contents");
+        AuthUser authUser = new AuthUser(1L, "email", UserRole.USER);
+        User user = User.fromAuthUser(authUser);
+        Todo todo = new Todo("title", "title", "contents", user);
+        Comment comment = new Comment(request.getContents(), user, todo);
+
+        given(todoRepository.findById(anyLong())).willReturn(Optional.of(todo));
+        given(commentRepository.save(any())).willReturn(comment);
+
+        // when
+        CommentSaveResponse result = commentService.saveComment(authUser, todoId, request);
+
+        // then
+        verify(commentRepository).save(any(Comment.class));
+        assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("실패 - 존재하지 않는 TODO")
+    public void saveComment_failed_by_todo_not_found() {
         // given
         long todoId = 1;
         CommentSaveRequest request = new CommentSaveRequest("contents");
@@ -52,23 +83,44 @@ class CommentServiceTest {
         assertEquals("Todo not found", exception.getMessage());
     }
 
+    // --------------------------------------
+
     @Test
-    public void comment를_정상적으로_등록한다() {
-        // given
-        long todoId = 1;
-        CommentSaveRequest request = new CommentSaveRequest("contents");
-        AuthUser authUser = new AuthUser(1L, "email", UserRole.USER);
-        User user = User.fromAuthUser(authUser);
-        Todo todo = new Todo("title", "title", "contents", user);
-        Comment comment = new Comment(request.getContents(), user, todo);
+    @DisplayName("성공 - 댓글 조회")
+    public void getComments_succeed(){
+        //given
+        long todoId = 1L;
+        User user = new User("user1@example.com", "password", UserRole.USER);
+        Todo todo = new Todo("Title", "Contents", "Sunny", user);
 
-        given(todoRepository.findById(anyLong())).willReturn(Optional.of(todo));
-        given(commentRepository.save(any())).willReturn(comment);
+        Comment mockComment = new Comment("테스트 내용", user, todo);
+        List<Comment> commentList = List.of(mockComment);
 
-        // when
-        CommentSaveResponse result = commentService.saveComment(authUser, todoId, request);
+        when(commentRepository.findByTodoIdWithUser(todoId)).thenReturn(commentList);
 
-        // then
-        assertNotNull(result);
+        //when
+        List<CommentResponse> responses = commentService.getComments(todoId);
+
+        //then
+        verify(commentRepository).findByTodoIdWithUser(todoId);
+        assertEquals(1, responses.size());
+    }
+
+    @Test
+    @DisplayName("성공 - 빈 commentList")
+    public void getComments_succeed_empty(){
+        //given
+        long todoId = 1L;
+        User user = new User("user1@example.com", "password", UserRole.USER);
+        Todo todo = new Todo("Title", "Contents", "Sunny", user);
+
+        when(commentRepository.findByTodoIdWithUser(todoId)).thenReturn(Collections.emptyList());
+
+        //when
+        List<CommentResponse> responses = commentService.getComments(todoId);
+
+        //then
+        verify(commentRepository).findByTodoIdWithUser(todoId);
+        assertThat(responses).isEmpty();
     }
 }
